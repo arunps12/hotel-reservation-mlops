7522029debcaff5f4b7265944c7d383b585a2c68
arunps12 <mail.arun1289@gmail.com>
2025-12-19 12:39:07 +0100

    feat: Implement data ingestion component for hotel reservation ML pipeline
    - Added DataIngestion class to handle downloading and splitting data from GCP.
    - Created configuration file (config.yaml) for data ingestion parameters.
    - Introduced paths_config.py for managing file paths.
    - Implemented custom exception handling with CustomException class.
    - Set up logging functionality with logger.py.
    - Added utility functions for reading YAML files and loading data.
    - Updated main.py to integrate the data ingestion process.
    - Added pyyaml dependency to pyproject.toml and requirements.txt.

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from hotel-reservation-mlops!")
+import sys
 
+from hotel_reservation_mlops.logging.logger import get_logger
+from hotel_reservation_mlops.exception.custom_exception import CustomException
+from hotel_reservation_mlops.components.data_ingestion import DataIngestion
+from hotel_reservation_mlops.utils.common_functions import read_yaml
+from hotel_reservation_mlops.config.paths_config import *
 
+logger = get_logger(__name__)
 if __name__ == "__main__":
-    main()
+    try:
+        # ============================================================
+        # 1) DATA INGESTION
+        # ============================================================
+        logger.info("=== hotel-reservation-mlops: Data Ingestion Pipeline Started ===")
+
+        data_ingestion = DataIngestion(read_yaml(CONFIG_PATH))
+        data_ingestion.run()
+        logger.info("Data ingestion completed successfully.")
+        
+
+        logger.info("=== hotel-reservation-mlops: Data Ingestion Pipeline Finished ===")
+    except Exception as e:
+        logger.error(f"An error occurred in the Data Ingestion Pipeline: {e}")
+        raise CustomException(e, sys)
